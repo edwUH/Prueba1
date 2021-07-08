be6c354b4bd9769720df3d2dd5387c1248f893ee
edwUH <85911899+edwUH@users.noreply.github.com>
2021-07-07 21:08:41 -0600

    Create Practica 1.R

---
+++ Practica 1.R
@@ -0,0 +1,79 @@
+#If
+Nota = 70
+
+if(Nota >=70) {
+  
+  print ("Aprobado") 
+  
+}else{
+  ("Reprobado")
+}
+
+#for
+Final=6
+
+for (i in 1:Final) {
+  if (i>=7) {
+    print("Estoy aprobado")
+  }
+}
+
+#integer
+auto=1000L
+Casa=2000L
+auto+Casa
+
+#double
+M=700000
+N=300000
+O=sqrt(M+N)
+paste("Valor de O es:",O)
+
+#caracter
+print("Ciencias de datos es solo para gatos")
+
+#vector
+
+V=c(1,2,3,4,5,6,7:25)
+class(V)
+V
+
+VectorMes = c ("enero","febrero", "marzo", "Abril","Mayo","Junio","Julio","Agosto", "Septiembre","Octubre","Noviembre","Diciembre")
+VectorMes
+class(VectorMes)
+
+VPersona = c("Emma","Jennifer","Gal","Fran")
+VPersona
+class(VPersona)
+
+View(data.frame(VPersona))
+
+Vector1 = c ("Shreck","Shreck 2","Shreck tercero","Shreck:Felices para siempre")
+Vector1
+
+Vector2 = c (7.9,7.2,6.1,6.3)
+Vector2
+
+VectorEstreno = c (TRUE, TRUE, FALSE, FALSE)
+VectorEstreno
+
+
+length (Vector2)
+Min (Vector2)
+Max (Vector2)
+sum (Vector2)
+mean (Vector2)
+median (Vector2)
+sort (Vector2)
+plot (Vector2)
+unique(Vector2)
+which.max (Vector2)
+sqrt (Vector2)
+
+VectorSeq=1:100
+
+VectorSequencia = seq (0,100, by =10)
+
+
+
+
